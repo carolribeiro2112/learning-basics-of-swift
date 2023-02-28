24d50b75a28367ef498b4ea394b6a36eba1712ac
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2023-02-28 19:05:07 -0300

    functions and closures

--- introSwift.playground/Contents.swift
+++ introSwift.playground/Contents.swift
@@ -132,3 +132,89 @@ for i in 0...4 {
     newTotal += i
 }
 print(newTotal)
+// end Control flow
+
+// Begin functions and closures
+
+// inicia função com a palavra reservada func
+// função com retorno precisa colocar uma arrow antes da chaves
+//func greet(person: String, day: String) -> {}
+
+func greet(person: String, day: String) -> String {
+    return "Hello \(person), today is \(day)"
+}
+greet(person: "Bob", day: "Tuesday")
+
+func greet(_ person: String, on day: String) -> String {
+    return "Hello \(person), today is \(day)"
+}
+greet("John", on: "Wednesday")
+
+// tupla: função retorna mais de um elemento
+func calculate(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+    
+    for score in scores {
+        if score > max {
+            max = score
+        } else if score < min {
+            min = score
+        }
+        sum += score
+    }
+        return (min, max, sum)
+}
+
+let values = calculate(scores: [5, 3, 100, 3, 9])
+print(values.sum)
+print(values.2)
+
+// funções aninhadas
+func return15() -> Int {
+    var y = 10
+    func add() {
+        y += 5
+    }
+    add()
+    return y
+}
+return15()
+
+func makeIncrement() -> ((Int) -> Int) {
+    func addOne(number: Int) -> Int {
+        return 1 + number
+    }
+    return addOne
+}
+
+var increment = makeIncrement()
+increment(7)
+
+func hasAnyMatches(list: [Int], condition: (Int)-> Bool) -> Bool {
+    for item in list {
+        if condition(item) {
+            return true
+        }
+    }
+    return false
+}
+
+func lessThan10(number: Int) -> Bool {
+    return number < 10
+}
+var numbers = [20, 19, 7, 12]
+hasAnyMatches(list:numbers, condition: lessThan10)
+
+// closures
+numbers.map({ (number: Int) -> Int in
+    let result = 3 * number
+    return result
+})
+
+let mapNumbers = numbers.map({number in 3 * number })
+print(mapNumbers)
+
+let sortedNumbers = numbers.sorted { $0 > $1 }
+print(sortedNumbers)
